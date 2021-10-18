d2665e94af14df38c8c1eb88603a5c4ad10a5e24
Shahriar Hossain <shahriarhossain9007@gmail.com>
2021-10-18 10:59:25 +0600

    Update rtsp.py

--- rtsp.py
+++ rtsp.py
@@ -1,8 +1,7 @@
 import cv2
 import os
  
-#RTSP_URL = 'rtsp://user:pass@192.168.0.189:554/h264Preview_01_main'
-RTSP_URL = cv2.VideoCapture('rtsp://user1:User123456@192.168.0.238:554/cam/realmonitor?channel=1&subtype=0')
+RTSP_URL = cv2.VideoCapture('rtsp://username:Users password@ip address:port/streaming/channels<id>')
 
 
 while True:
